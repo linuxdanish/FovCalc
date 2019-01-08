f560b32fe9e116c0cd31b30a9e15c5648951d695
Daniel Tate <daniellawrencetate@gmail.com>
2019-01-08 00:22:11 -0500

    Basic field of view calculator

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -9,9 +9,32 @@ namespace FovCalc.Pages
 {
     public class IndexModel : PageModel
     {
+
+        [BindProperty]
+        public double senW { get; set; }
+
+        [BindProperty]
+        public double fL { get; set; }
+
+        [BindProperty]
+        public double dist { get; set; }
+
+        [BindProperty]
+        public double linearFovW { get; set; }
+
+        public double anamorphFov;
+
+        public double AoV;
         public void OnGet()
         {
 
+        }
+
+        public void OnPost() 
+        {
+            linearFovW = (senW/fL) * dist;
+            anamorphFov = linearFovW * 2;
+            AoV = 2 * Math.Atan(senW / ( 2 * fL)) * (180/Math.PI);
         }
     }
 }
