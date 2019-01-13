469836c4fef332a1f16807e0368622ef28a5124b
Daniel Tate <daniellawrencetate@gmail.com>
2019-01-12 23:46:26 -0500

    Convert sensors to drop down of cameras

---
+++ FrameSizes.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace FovCalc
+{
+    public class Camera
+    {
+        public string name { get; set; }
+
+        public double sensorW { get; set; }
+
+        public double sensorH { get; set; }
+    }
+} 
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FovCalc.Pages
 {
     public class IndexModel : PageModel
     {
 
+        public SelectList frameSizes { get; set; }
+
+       static private Dictionary<string, Camera> cameras = new Dictionary<string, Camera>()
+        {
+            {"GH5s", new Camera{ name = "GH5s", sensorH = 13, sensorW = 17.3}},
+            {"35mm", new Camera{ name = "Super35 Film", sensorH = 24.89, sensorW = 18.66}},
+            {"Alexa", new Camera{ name = "Arri Alexa 4:3", sensorH = 23.76, sensorW = 17.82}}
+        };
+
+        public SelectList cameraOptions { get; set; }
+
         [BindProperty]
         public double senW { get; set; }
+
+        [BindProperty]
+        public double senH { get; set; }
+
+        [BindProperty]
+        public string camera { get; set; }
 
         [BindProperty]
         public double fL { get; set; }
@@ -22,16 +40,29 @@ namespace FovCalc.Pages
         [BindProperty]
         public double linearFovW { get; set; }
 
+        [BindProperty]
+        public bool anamorphic { get; set; }
+
         public double anamorphFov;
 
         public double AoV;
         public void OnGet()
         {
+            frameSizes = new SelectList(cameras, "Key", "Value");
 
+            cameraOptions = new SelectList
+            (
+                cameras.Select( x => new { Value = x.Key, Text = x.Value.name}),
+                "Value",
+                "Text"
+            );
         }
 
         public void OnPost() 
         {
+            senH = cameras[camera].sensorH;
+            senW = cameras[camera].sensorW;
+
             linearFovW = (senW/fL) * dist;
             anamorphFov = linearFovW * 2;
             AoV = 2 * Math.Atan(senW / ( 2 * fL)) * (180/Math.PI);
